0ba46edf7353a50e58345bf8563b7f3168be1806
Viktor Ödman <vo222dq@student.lnu.se>
2020-03-26 12:50:51 +0100

    adding gitignore

---
+++ DescriptiveStatistics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DescriptiveStatistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
