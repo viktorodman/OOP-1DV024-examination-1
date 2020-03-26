d90a984430bab544ac49a766b442cb064ed3143e
Viktor Ödman <vo222dq@student.lnu.se>
2020-03-26 18:37:10 +0100

    Adding class DescriptiveStatistics

---
+++ DescriptiveStatistics/Statistics.cs
@@ -0,0 +1,41 @@
+namespace DescriptiveStatistics
+{
+    static class Statistics
+    {
+        private static dynamic DescriptiveStatistics(int[] source)
+        {
+
+        }
+
+        private static int Maximum (int[] source)
+        {
+
+        }
+        private static double Mean (int[] source)
+        {
+
+        }
+        private static double Median (int[] source)
+        {
+
+        }
+        private static int Minimum (int[] source)
+        {
+
+        }
+        private static int[] Mode (int[] source)
+        {
+
+        }
+        private static int Range (int[] source)
+        {
+
+        }
+        private static double StandardDeviation (int[] source)
+        {
+
+        }
+
+        
+    }
+}
